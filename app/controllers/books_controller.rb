39fdbf925105bf28e0538fcb95d76a12e4a2ae52
konno57 <126854839+konno57@users.noreply.github.com>
2023-03-25 08:54:52 +0000

    [Update]bookのindexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,16 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.order(:id)
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+      @book = Book.new(book_params)
+      @book.user_id = current_user.id
+      @book.save
+      redirect_to '/books'
   end
 
   def show
@@ -11,4 +21,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to @user
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
   end
 end
